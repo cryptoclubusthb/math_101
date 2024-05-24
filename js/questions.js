a02dd03eec87fb294a76da42bcd08846933af17c
lounesbbkr <99367325+lounesbbkr@users.noreply.github.com>
2024-05-24 18:11:17 +0100

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the result of 5 + 7?",
+    answer: "12",
+    options: [
+      "10",
+      "11",
+      "12",
+      "13"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Solve for x: 2x + 3 = 11",
+    answer: "x = 4",
+    options: [
+      "x = 2",
+      "x = 3",
+      "x = 4",
+      "x = 5"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the area of a rectangle with length 8 and width 5?",
+    answer: "40 square units",
+    options: [
+      "13 square units",
+      "30 square units",
+      "35 square units",
+      "40 square units"
+    ]
+  },
+  {
+    numb: 4,
+    question: "If a triangle has sides of length 5, 12, and 13 units, what type of triangle is it?",
+    answer: "Right triangle",
+    options: [
+      "Equilateral triangle",
+      "Isosceles triangle",
+      "Scalene triangle",
+      "Right triangle"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the square root of 81?",
+    answer: "9",
+    options: [
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  }
+];
